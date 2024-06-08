f13d88450784b686b2ca0928d35d1b65d3ad76e6
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-08 23:26:04 +0530

    Update api.config.js

--- frontend/src/config/api.config.js
+++ frontend/src/config/api.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5834";
+export const API_BASE_URL = "https://e-commerce-website-clj5.onrender.com";
 
 const jwt = localStorage.getItem("jwt")
 
